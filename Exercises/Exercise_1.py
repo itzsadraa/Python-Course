302521289ad2411a5296db60189203b8e2cdb3c1
nUx <72265156+RetrO-13th@users.noreply.github.com>
2024-04-25 15:25:29 +0330

    Add Exercise_1.py

---
+++ Exercises/Exercise_1.py
@@ -0,0 +1,10 @@
+# Calculate the multiplication and sum of two numbers
+
+number1 = 20
+number2 = 30
+
+# multiplication
+print(f"multiplication = {number1 * number2}")
+
+# sum
+print(f"sum = {number1 + number2}")
